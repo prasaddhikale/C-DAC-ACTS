47a583ca1c47f1e6c89917f6b9f04d9d34ea5bbf
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-03-07 23:30:29 +0530

    Add files via upload

---
+++ Daywise Lab Work/Object Oriented Programming With Java/Assignment 4/core/B.java
@@ -0,0 +1,18 @@
+package com.cdac.core;
+
+public class B {
+	String s;
+	int i;
+	
+	public  B(String s,int i) {
+		this.s=s;
+		this.i=i;
+		
+	}
+	
+	public void display() {
+		System.out.println(s);
+		System.out.println(i);
+	}
+}
+
---
+++ Daywise Lab Work/Object Oriented Programming With Java/Assignment 4/tester/B_Tester.java
@@ -0,0 +1,14 @@
+package com.cdac.tester;
+
+import com.cdac.core.A;
+import com.cdac.core.B;
+
+public class B_Tester {
+	public static void main(String[] args) {
+		B b1 = new B("harsh",23);
+		A b2 = new A("Hp",6);
+		b1.display();
+		b2.display();
+
+}
+}
