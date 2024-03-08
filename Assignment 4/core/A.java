da37f175889981170b4467c21e4495326ac0cee6
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-03-09 00:10:08 +0530

    Add files via upload

---
+++ Assignment 4/core/A.java
@@ -0,0 +1,17 @@
+package com.cdac.core;
+
+
+	public class A extends B{
+		public A (String s,int i) {
+			super(s,i);
+			
+		}
+	
+
+	public void display() {
+		System.out.println(s);
+		System.out.println(i);
+	}
+	
+}
+
---
+++ Assignment 4/core/Tank.java
@@ -0,0 +1,16 @@
+package com.cdac.core;
+
+public class Tank {
+	private int level;
+	
+	public Tank(int l){
+		level=l;
+	}
+	public void setLevel(int level1) {
+		level=level1;
+	}
+	public int getLevel() {
+		return level;
+	}
+
+}
