10af171552c7d818d91c71aa3636666abdccd25d
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-04-17 11:45:28 +0530

    Add files via upload

---
+++ Daywise Lab Work/WPT/NodeJs/NodeJsLabE/userDb.js
@@ -0,0 +1,4 @@
+exports.users = [{username:"user123", password:"user"},
+                {username:"test234", password:"test"},
+                {username:"test123",password:"test"}
+                ]
