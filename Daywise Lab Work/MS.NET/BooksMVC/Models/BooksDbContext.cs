cde09516ac6fa575453ec112557f337cc3af264f
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-04-26 23:53:44 +0530

    Add files via upload

---
+++ Daywise Lab Work/MS.NET/BooksMVC/Models/BooksDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksMVC.Models;
+
+public partial class BooksDbContext : DbContext
+{
+    public BooksDbContext()
+    {
+    }
+
+    public BooksDbContext(DbContextOptions<BooksDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblBook> TblBooks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MsSqlLocalDb;Initial Catalog=BooksDB;Integrated Security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblBook>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK__tbl_book__3DE0C227F24974C3");
+
+            entity.ToTable("tbl_books");
+
+            entity.Property(e => e.BookId)
+                .ValueGeneratedNever()
+                .HasColumnName("BookID");
+            entity.Property(e => e.BookAuthor)
+                .HasMaxLength(20)
+                .IsFixedLength();
+            entity.Property(e => e.BookName)
+                .HasMaxLength(20)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
