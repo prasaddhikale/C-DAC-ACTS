317186a7c5205c91703bf8e94bcea6ac12c7f0aa
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-04-30 23:46:52 +0530

    Add files via upload

---
+++ Daywise Lab Work/MS.NET/EmployeeAPI/Controllers/TblEmployeesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmployeeAPI.Models;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TblEmployeesController : ControllerBase
+    {
+        private readonly EmployeesMvcContext _context;
+
+        public TblEmployeesController(EmployeesMvcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TblEmployees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblEmployee>>> GetTblEmployees()
+        {
+            return await _context.TblEmployees.ToListAsync();
+        }
+
+        //// GET: api/TblEmployees/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<TblEmployee>> GetTblEmployee(int id)
+        //{
+        //    var tblEmployee = await _context.TblEmployees.FindAsync(id);
+
+        //    if (tblEmployee == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return tblEmployee;
+        //}
+
+        //// PUT: api/TblEmployees/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutTblEmployee(int id, TblEmployee tblEmployee)
+        //{
+        //    if (id != tblEmployee.EmployeeId)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(tblEmployee).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!TblEmployeeExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// POST: api/TblEmployees
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<TblEmployee>> PostTblEmployee(TblEmployee tblEmployee)
+        //{
+        //    _context.TblEmployees.Add(tblEmployee);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetTblEmployee", new { id = tblEmployee.EmployeeId }, tblEmployee);
+        //}
+
+        //// DELETE: api/TblEmployees/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteTblEmployee(int id)
+        //{
+        //    var tblEmployee = await _context.TblEmployees.FindAsync(id);
+        //    if (tblEmployee == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.TblEmployees.Remove(tblEmployee);
+        //    await _context.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        //private bool TblEmployeeExists(int id)
+        //{
+        //    return _context.TblEmployees.Any(e => e.EmployeeId == id);
+        //}
+    }
+}
---
+++ Daywise Lab Work/MS.NET/EmployeeAPI/Models/EmployeesMvcContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Models;
+
+public partial class EmployeesMvcContext : DbContext
+{
+    public EmployeesMvcContext()
+    {
+    }
+
+    public EmployeesMvcContext(DbContextOptions<EmployeesMvcContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblEmployee> TblEmployees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MsSqlLocalDb;Initial Catalog=EmployeesMVC;Integrated Security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblEmployee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__tbl_Empl__7AD04FF1F1B1E8E4");
+
+            entity.ToTable("tbl_Employees");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.EmployeeCity).HasMaxLength(50);
+            entity.Property(e => e.EmployeeDob).HasColumnName("EmployeeDOB");
+            entity.Property(e => e.EmployeeGender).HasMaxLength(10);
+            entity.Property(e => e.EmployeeName).HasMaxLength(100);
+            entity.Property(e => e.EmployeeSalary).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
