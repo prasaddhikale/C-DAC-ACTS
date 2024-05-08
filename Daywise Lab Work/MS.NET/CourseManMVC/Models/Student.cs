06f9ccd99648e66c6cf542bd36aef8d9f254c7d2
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-05-08 21:11:38 +0530

    Add files via upload

---
+++ Daywise Lab Work/MS.NET/CourseManMVC/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseManMVC.Models
+{
+    public partial class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public int Marks { get; set; }
+
+        public int Age { get; set; }
+
+        public int CourseId { get; set; }
+
+        [ForeignKey("CourseId")]
+        public virtual Course? Course { get; set; } = null!;
+    }
+}
---
+++ Daywise Lab Work/MS.NET/CourseManMVC/Models/StudentResContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseManMVC.Models;
+
+public partial class StudentResContext : DbContext
+{
+    public StudentResContext()
+    {
+    }
+
+    public StudentResContext(DbContextOptions<StudentResContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MsSqlLocalDb;Initial Catalog=StudentRes;Integrated Security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.CourseId).HasName("PK__Course__C92D71878055D624");
+
+            entity.ToTable("Course");
+
+            entity.Property(e => e.CourseId)
+                .ValueGeneratedNever()
+                .HasColumnName("CourseID");
+            entity.Property(e => e.CourseName)
+                .HasMaxLength(10)
+                .IsFixedLength();
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC07C2F2F281");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.CourseId).HasColumnName("CourseID");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(20)
+                .IsFixedLength();
+            entity.Property(e => e.LastName)
+                .HasMaxLength(20)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Students)
+                .HasForeignKey(d => d.CourseId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Student_ToTable");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
