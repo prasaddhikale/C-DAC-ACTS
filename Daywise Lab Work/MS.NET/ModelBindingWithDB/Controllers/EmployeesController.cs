50b8a95cc0b90edfa6c356c33359db107234db1e
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-05-12 22:28:48 +0530

    Add files via upload

---
+++ Daywise Lab Work/MS.NET/ModelBindingWithDB/Controllers/EmployeesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingWithDB.Models;
+
+namespace ModelBindingWithDB.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: EmployeesController
+        public ActionResult Index()
+        {
+            List<Employee> list = Employee.GetAllEmployees();
+            return View(list);
+        }
+
+        // GET: EmployeesController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // GET: EmployeesController/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Employee Obj)
+        {
+            try
+            {
+                Employee.Insert(Obj);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: EmployeesController/Edit/5
+
+
+        public ActionResult Edit(int id)
+        {
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee obj)
+        {
+            try
+            {
+                Employee.Update(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(IFormCollection collection, int id)
+        {
+            try
+            {
+                Employee.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Daywise Lab Work/MS.NET/ModelBindingWithDB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelBindingWithDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
