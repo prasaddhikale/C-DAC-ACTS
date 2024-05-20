a2e522911e15f67c8cbe6c8b024e6183b7bd0dcb
Prasad <152061815+prasaddhikale@users.noreply.github.com>
2024-05-20 22:14:34 +0530

    Add files via upload

---
+++ Daywise Lab Work/MS.NET/LabExam_B1_052/EmployeeManageMentMVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,90 @@
+﻿using EmployeeManageMentMVCApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManageMentMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+
+            List<Employee> list = new List<Employee>();
+            list=Employee.GetAllEmployees();
+            return View(list);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection,Employee emp)
+        {
+            try
+            {
+                Employee.Insert(emp);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                Employee.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Daywise Lab Work/MS.NET/LabExam_B1_052/EmployeeManageMentMVCApp/Models/Employee.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManageMentMVCApp.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "First Name is required")]
+        public string Name { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        public static List<Employee> GetAllEmployees()
+        {
+            var employees = new List<Employee>();
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType= System.Data.CommandType.Text;
+                cmd.CommandText = "select * from Employees";
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    employees.Add(new Employee {Id=dr.GetInt32(0),Name=dr.GetString(1),City=dr.GetString(2),Address=dr.GetString(3) });
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            finally { conn.Close(); }
+            return employees;
+        }
+
+        public static void Insert(Employee emp)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "insert into Employees values(@Name,@City,@Address)";
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@City", emp.City);
+                cmd.Parameters.AddWithValue("@Address", emp.Address);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally { conn.Close(); }
+        }
+
+        public static Employee GetSingleEmployee(int id)
+        {
+            Employee obj = new Employee();
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select * from Employees where Id=@Id";
+                cmd.Parameters.AddWithValue("@Id", id);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if(dr.Read())
+                {
+                    obj.Id = dr.GetInt32(0);
+                    obj.Name = dr.GetString(1);
+                    obj.City = dr.GetString(2);
+                    obj.Address = dr.GetString(3);
+                }
+                else
+                {
+                    obj = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally { conn.Close(); }
+            return obj;
+        }
+
+        internal static void Delete(int id)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmdInsert = new SqlCommand();
+                cmdInsert.Connection = cn;
+                cmdInsert.CommandType = System.Data.CommandType.Text;
+                cmdInsert.CommandText = "delete from Employees where Id =@Id";
+                cmdInsert.Parameters.AddWithValue("@Id", id);
+                cmdInsert.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+
+
+}
